7c7c72ac8a2a4cbc5cdbdfb9dfb71f17a6b48985
Gareth Campling <gareth@darkstudios.co.uk>
2012-02-05 13:15:14 +0000

    Hyphen patch for as-stats 1.34

---
+++ README-linkgraph.php
@@ -0,0 +1,8 @@
+Patch for AS-Stats v1.34 (2011-04-26)
+a simple tool to generate per-AS traffic graphs from NetFlow/sFlow records
+by Manuel Kasper, Monzoon Networks AG <mkasper@monzoon.net>
+--------------------------------------------------------------------
+
+Changes
+-------
+fixes the linkgraphs page when hyphens are used in linkname
